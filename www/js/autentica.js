2f5678b3972c31036016bb33758fb284b95df475
ElviajeroO <joaohenrique010604@gmail.com>
2024-03-05 19:44:10 -0300

    js da autenticação do usuario se comunicando com o php, e criação do arquivo autentica.php

--- www/js/autentica.js
+++ www/js/autentica.js
@@ -1,5 +1,10 @@
 async function Autentica(){
 	var form = document.getElementById('form_cadastro');
 	var dados = new FormData(form);
-	console.log(dados);
+
+	var promise = await fetch('php/autentica.php',{
+		method:'POST',
+		body:dados
+	});
+
 }
