3b839fd1849f8f0311837c66740eea0867c570ad
Mateus Daniel <mateusdaniel2509@gmail.com>
2024-03-19 21:02:38 -0300

    Implementacao de verificacao de campos.
    Construido a verificacao de tamanho de senha, complexidade
    utilizando regex.

--- www/js/cadastra.js
+++ www/js/cadastra.js
@@ -1,10 +1,34 @@
-async function Cadastrar(){
+function Cadastrar(){
+	var senha1 = document.getElementById('senha1').value;
+	var senha2 = document.getElementById('senha2').value;
+
+	const regex0 = /\W|_/;
+	const regex1 = /[A-Z]/;
+	const regex2 = /[0-9]/;
+	const regex3 = /[a-z]/;
+
+	if (senha1 == senha2) {
+		if ( (regex0.test(senha1)) && (regex1.test(senha1)) && (regex2.test(senha1)) && (regex3.test(senha1)) ){
+			if (senha1.length >8) {
+				CadastrarTudo();
+			}else{
+				alert("A senha deve ter no minimo 9 caracteres");
+			}
+		}else{
+			alert("Tenha no minimo um: Caractere especial, letra G e P, e numero");
+		}
+	}else {
+		alert("ERRO: As duas senhas não batem");
+	}
+}
+
+async function CadastrarTudo(){
+	alert("chegamos ai fim");
 	var form = document.getElementById('form_cadastro');
-	var dados = new FormData(form);
-
-	var promise = await fetch('../php/cadastra.php',{
-		method:'POST',
-		body:dados
-	});
-
+		var dados = new FormData(form);
+	
+		var promise = await fetch('../php/cadastra.php',{
+			method:'POST',
+			body:dados
+		});
 }
