290b2ad16355339b3ab25a1088fe7d38fe1c0bc5
Mateus Daniel <mateusdaniel2509@gmail.com>
2024-04-02 21:55:02 -0300

    Correção do PHPMailer, em total funcionamento.
    Foram feitas mudanças e correções para o funcionamento do
    PHPMailer. Está funcionando corretamente enviando o código de
    confirmação no email cadastrado pelo usuario.

--- php/phpmailer.php
+++ php/phpmailer.php
@@ -7,28 +7,29 @@
     require 'PHPMailer/src/Exception.php';
     require 'PHPMailer/src/PHPMailer.php';
     require 'PHPMailer/src/SMTP.php';
+    function mandar($email, $codigo_aut){ 
 
-    $mail = new PHPMailer();
+        $mail = new PHPMailer();
 
-    $mail->Mailer = "smtp";
-    $mail->isSMTP();
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPDebug= 0;
-    $mail->SMTPAuth= true;
-    $mail->SMTPSecure= 'ssl';
-    $mail->Host= 'smtp.gmail.com';
-    $mail->Port= 465;
+        $mail->Mailer = "smtp";
+        $mail->isSMTP();
+        $mail->CharSet = "UTF-8";
+        $mail->SMTPDebug= 0;
+        $mail->SMTPAuth= true;
+        $mail->SMTPSecure= 'ssl';
+        $mail->Host= 'smtp.gmail.com';
+        $mail->Port= 465;
 
-    $mail->Username= 'mateustrabalhoexp';
-    $mail->Password= 'ujjc kllj upvo otag';
-    $mail->setFrom('mateustrabalhoexp@gmail.com', 'EMPRESA');
-    
-    $mail->addAddress('mateusdaniel2509@gmail.com', '');
-    $mail->Subject = "SEXOOOOOOOOOOOOO";
+        $mail->Username= 'mateustrabalhoexp';
+        $mail->Password= 'ujjc kllj upvo otag';
+        $mail->setFrom('mateustrabalhoexp@gmail.com', 'EMPRESA');
+        
+        $mail->addAddress($email, '');
+        $mail->Subject = "Código de Autenticação";
 
-    $mail->msgHTML("Mensagem");
+        $mail->msgHTML($codigo_aut);
 
-    $mail->send();
-
+        $mail->send();
+    }
 
 ?>
