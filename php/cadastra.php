d94e01a0f75f7793f5fc5e1faa49cd9ff66660be
Mateus Daniel <mateusdaniel2509@gmail.com>
2024-04-02 21:52:55 -0300

    Gerando numero aleatorio para codigo de confirmacao
    Foi gerado o codigo de confirmacao de usuario, atraves da
    função "random_int", o qual é adicionado a uma coluna no DB
    e em seguida enviado ao "phpmailer.php" para envio ao cliente.

--- php/cadastra.php
+++ php/cadastra.php
@@ -1,8 +1,12 @@
 <?php
+	include "phpmailer.php";
 	$email = $_POST['email'];
 	$senha = $_POST['senha'];
-	
+    $codigo_aut = abs(random_int(-9999, 9999));
+	echo($codigo_aut);
+
 	$connection = mysqli_connect('127.0.0.1:3306', 'root', 'root', 'web');
-	$query = "INSERT INTO users (email, senha) VALUES ('$email', '$senha')";
+	$query = "INSERT INTO users (email, senha ,codconfirmacao) VALUES ('$email', '$senha', '$codigo_aut')";
 	mysqli_query($connection, $query);
+	mandar($email, $codigo_aut);
 ?>
