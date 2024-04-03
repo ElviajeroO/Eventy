63d25ad00b020236caf1ff8cd570e59cc33ccc71
Mateus Daniel <mateusdaniel2509@gmail.com>
2024-04-02 22:30:26 -0300

    Subindo versionamento do codigo que esta rodando

--- php/cadastra.php
+++ php/cadastra.php
@@ -3,7 +3,6 @@
 	$email = $_POST['email'];
 	$senha = $_POST['senha'];
     $codigo_aut = abs(random_int(-9999, 9999));
-	echo($codigo_aut);
 
 	$connection = mysqli_connect('127.0.0.1:3306', 'root', 'root', 'web');
 	$query = "INSERT INTO users (email, senha ,codconfirmacao) VALUES ('$email', '$senha', '$codigo_aut')";
